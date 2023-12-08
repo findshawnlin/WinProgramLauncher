4f4ad701e583b7f610c68f43f55b1ddc7ac22a49
findshawnlin <45290585+findshawnlin@users.noreply.github.com>
2023-12-08 19:16:07 +0800

    2stsub

---
+++ README.md
@@ -0,0 +1,46 @@
+# Win Program Launcher
+
+This is a simple Win Program Launcher designed to demonstrate basic functionality using PyQt5 in Python.
+
+## Features
+
+- Three main buttons, each with an expandable set of sub-buttons.
+- Clicking on a sub-button launches the corresponding executable file.
+- Customizable button styles and colors.
+
+## Prerequisites
+
+- Python 3.x
+- PyQt5 library
+
+## Getting Started
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/your-username/win-program-launcher.git
+
+2. Install the required dependencies:
+   ```bash
+   pip install PyQt5
+
+3. Run the program:
+   ```bash
+   python main.py
+
+## Usage
+- Click on the main buttons to expand or collapse the set of sub-buttons.
+- Click on a sub-button to launch the associated executable file.
+
+## Customization
+- You can customize the button styles, colors, and executable paths in the main.py file.
+
+## Issues and Contributions
+- If you encounter any issues or have suggestions for improvements, please open an issue.
+- Feel free to contribute by submitting a pull request.
+
+## License
+- This project is licensed under the MIT License - see the LICENSE file for details.
+- Replace "your-username" and adjust any other details as needed. This template includes sections for features, prerequisites, getting started, usage, customization, issues, contributions, and licensing. You can add or modify sections based on the specifics of your Win Program Launcher.
+
+
+
--- wpl.py
+++ wpl.py
@@ -8,41 +8,41 @@ class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.sub_buttons_main = []  # 存储主按钮下的子按钮的列表
-        self.sub_buttons_main2 = []  # 存储主按钮2下的子按钮的列表
-        self.is_main_button_expanded = False  # 记录主按钮的展开状态
-        self.is_main_button2_expanded = False  # 记录主按钮2的展开状态
+        self.sub_buttons_main = []  # List to store sub-buttons under the main button
+        self.sub_buttons_main2 = []  # List to store sub-buttons under the main button 2
+        self.is_main_button_expanded = False  # Records the expansion state of the main button
+        self.is_main_button2_expanded = False  # Records the expansion state of the main button 2
         self.initUI()
 
-    # 初始化界面
+    # Initialize the interface
     def initUI(self):
         self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('PyQt5 示例')
+        self.setWindowTitle('PyQt5 Example')
 
-        # 创建主按钮并连接槽函数
+        # Create the main button and connect it to the slot function
         self.main_button = QPushButton(self)
-        self.setupButton(self.main_button, '主按钮', self.toggleSubButtonsMain, '#4CAF50')  # 绿色
+        self.setupButton(self.main_button, 'Main Button', self.toggleSubButtonsMain, '#4CAF50')  # Green
 
-        # 创建主按钮2并连接槽函数
+        # Create the main button 2 and connect it to the slot function
         self.main_button2 = QPushButton(self)
-        self.setupButton(self.main_button2, '主按钮2', self.toggleSubButtonsMain2, '#2196F3')  # 蓝色
+        self.setupButton(self.main_button2, 'Main Button 2', self.toggleSubButtonsMain2, '#2196F3')  # Blue
 
-        # 创建垂直布局
+        # Create a vertical layout
         self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.main_button)  # 将主按钮添加到布局中
-        self.vbox.addWidget(self.main_button2)  # 将主按钮2添加到布局中
+        self.vbox.addWidget(self.main_button)  # Add the main button to the layout
+        self.vbox.addWidget(self.main_button2)  # Add the main button 2 to the layout
 
-        # 设置布局
+        # Set the layout
         self.setLayout(self.vbox)
 
-    # 设置按钮属性和样式
+    # Set button properties and styles
     def setupButton(self, button, text, slot, color):
         button.setText(text)
-        button.setIcon(QIcon('icon.png'))  # 请替换 'icon.png' 为你的图标文件路径
+        button.setIcon(QIcon('icon.png'))  # Replace 'icon.png' with your icon file path
         button.setIconSize(button.sizeHint() / 2)
         button.clicked.connect(slot)
 
-        # 设置按钮样式
+        # Set button styles
         button.setStyleSheet(f'''
             QPushButton {{
                 background-color: {color};
@@ -56,123 +56,123 @@ class MyWidget(QWidget):
             }}
 
             QPushButton:hover {{
-                background-color: #1E88E5; /* 深蓝色 */
+                background-color: #1E88E5; /* Dark blue */
             }}
         ''')
 
-    # 切换主按钮下的子按钮的显示状态
+    # Toggle the display status of sub-buttons under the main button
     def toggleSubButtonsMain(self):
         if not self.is_main_button_expanded:
             self.showSubButtonsMain()
         else:
             self.hideSubButtonsMain()
 
-    # 切换主按钮2下的子按钮的显示状态
+    # Toggle the display status of sub-buttons under the main button 2
     def toggleSubButtonsMain2(self):
         if not self.is_main_button2_expanded:
             self.showSubButtonsMain2()
         else:
             self.hideSubButtonsMain2()
 
-    # 显示主按钮下的子按钮
+    # Display sub-buttons under the main button
     def showSubButtonsMain(self):
-        # 移除所有按钮
+        # Remove all buttons
         self.clearButtons()
 
-        # 创建四个子按钮并连接槽函数
+        # Create four sub-buttons and connect them to the slot functions
         sub_button1 = QPushButton(self)
-        self.setupButton(sub_button1, '子按钮1', lambda: self.openExecutable('path/to/executable1'), '#FFC107')  # 黄色
+        self.setupButton(sub_button1, 'Sub Button 1', lambda: self.openExecutable('path/to/executable1'), '#FFC107')  # Yellow
 
         sub_button2 = QPushButton(self)
-        self.setupButton(sub_button2, '子按钮2', lambda: self.openExecutable('path/to/executable2'), '#FF5722')  # 橙色
+        self.setupButton(sub_button2, 'Sub Button 2', lambda: self.openExecutable('path/to/executable2'), '#FF5722')  # Orange
 
         sub_button3 = QPushButton(self)
-        self.setupButton(sub_button3, '子按钮3', lambda: self.openExecutable('path/to/executable3'), '#E91E63')  # 粉红色
+        self.setupButton(sub_button3, 'Sub Button 3', lambda: self.openExecutable('path/to/executable3'), '#E91E63')  # Pink
 
         sub_button4 = QPushButton(self)
-        self.setupButton(sub_button4, '子按钮4', lambda: self.openExecutable('path/to/executable4'), '#795548')  # 棕色
+        self.setupButton(sub_button4, 'Sub Button 4', lambda: self.openExecutable('path/to/executable4'), '#795548')  # Brown
 
-        # 将子按钮添加到列表中，方便后续管理
+        # Add sub-buttons to the list for later management
         self.sub_buttons_main.extend([sub_button1, sub_button2, sub_button3, sub_button4])
 
-        # 将所有按钮重新添加到布局中
+        # Add all buttons back to the layout
         self.addButtons()
 
-        # 更新展开状态
+        # Update the expansion state
         self.is_main_button_expanded = True
 
-    # 显示主按钮2下的子按钮
+    # Display sub-buttons under the main button 2
     def showSubButtonsMain2(self):
-        # 移除所有按钮
+        # Remove all buttons
         self.clearButtons()
 
-        # 创建一个子按钮并连接槽函数
+        # Create one sub-button and connect it to the slot function
         sub_button_main2 = QPushButton(self)
-        self.setupButton(sub_button_main2, '主按钮2下的子按钮', lambda: self.openExecutable('path/to/executable_main2'), '#9C27B0')  # 紫色
+        self.setupButton(sub_button_main2, 'Sub Button under Main Button 2', lambda: self.openExecutable('path/to/executable_main2'), '#9C27B0')  # Purple
 
-        # 创建另一个子按钮
+        # Create another sub-button
         sub_button2 = QPushButton(self)
-        self.setupButton(sub_button2, '额外的子按钮1', lambda: self.openExecutable("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"), '#009688')  # 青色
+        self.setupButton(sub_button2, 'Additional Sub Button 1', lambda: self.openExecutable("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"), '#009688')  # Cyan
 
         sub_button3 = QPushButton(self)
-        self.setupButton(sub_button3, '额外的子按钮2', lambda: self.openExecutable("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"), '#607D8B')  # 灰色
+        self.setupButton(sub_button3, 'Additional Sub Button 2', lambda: self.openExecutable("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"), '#607D8B')  # Gray
 
-        # 将子按钮添加到列表中，方便后续管理
+        # Add sub-buttons to the list for later management
         self.sub_buttons_main2.extend([sub_button_main2, sub_button2, sub_button3])
 
-        # 将所有按钮重新添加到布局中
+        # Add all buttons back to the layout
         self.addButtons()
 
-        # 更新展开状态
+        # Update the expansion state
         self.is_main_button2_expanded = True
 
-    # 隐藏主按钮下的子按钮
+    # Hide sub-buttons under the main button
     def hideSubButtonsMain(self):
         for sub_button in reversed(self.sub_buttons_main):
             sub_button.hide()
 
-        # 更新展开状态
+        # Update the expansion state
         self.is_main_button_expanded = False
 
-    # 隐藏主按钮2下的子按钮
+    # Hide sub-buttons under the main button 2
     def hideSubButtonsMain2(self):
         for sub_button in reversed(self.sub_buttons_main2):
             sub_button.hide()
 
-        # 更新展开状态
+        # Update the expansion state
         self.is_main_button2_expanded = False
 
-    # 清除所有按钮
+    # Clear all buttons
     def clearButtons(self):
         for sub_button in self.sub_buttons_main + self.sub_buttons_main2:
             sub_button.setParent(None)
 
-    # 将所有按钮重新添加到布局中
+    # Add all buttons back to the layout
     def addButtons(self):
-        # 获取主按钮的索引
+        # Get the index of the main button
         main_button_index = self.vbox.indexOf(self.main_button)
 
-        # 添加主按钮下的子按钮
+        # Add sub-buttons under the main button
         for sub_button in self.sub_buttons_main:
             main_button_index += 1
             self.vbox.insertWidget(main_button_index, sub_button)
 
-        # 获取主按钮2的索引
+        # Get the index of the main button 2
         main_button2_index = self.vbox.indexOf(self.main_button2)
 
-        # 添加主按钮2下的子按钮
+        # Add sub-buttons under the main button 2
         for sub_button in self.sub_buttons_main2:
             main_button2_index += 1
             self.vbox.insertWidget(main_button2_index, sub_button)
 
-    # 打开可执行文件
+    # Open the executable file
     def openExecutable(self, executable_path):
         try:
             subprocess.Popen([executable_path])
         except Exception as e:
             print(f"Error opening executable: {e}")
 
-# 主程序入口
+# Main program entry point
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     my_widget = MyWidget()
